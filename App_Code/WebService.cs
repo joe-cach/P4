7dc3c0949acaa48e7303620e247836c4c723d336
joe <joseph.cacharani@gmail.com>
2021-10-04 08:17:32 -0400

    p4

---
+++ App_Code/WebService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Descripción breve de WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hola a todos";
+    }
+
+    public string HelloWorld2()
+    {
+        return "Hola Moi";
+    }
+
+    [WebMethod]
+    public int suma(int a, int b)
+    {
+        return a+b;
+    }
+
+    [WebMethod]
+    public DataSet alumno()
+    {
+        SqlConnection con = new SqlConnection();
+        SqlDataAdapter ada = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        con.ConnectionString = "server=(local);user=usuario324;pwd=123456;database=academico324";
+        ada.SelectCommand = new SqlCommand();
+        ada.SelectCommand.Connection = con;
+        ada.SelectCommand.CommandText =  "select * from alumno";
+        ada.Fill(ds);
+        return ds;
+    }
+}
